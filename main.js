e5814d6a7316e97210a78202f9edb86cf744a745
brunoheroesevolved@gmail.com <brunoheroesevolved@gmail.com>
2022-06-30 11:45:15 -0300

    toDolist/DOMManipulation

---
+++ main.js
@@ -0,0 +1,29 @@
+const ulElement = document.querySelector("ul");
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("form button")
+const tasks = []
+
+buttonElement.onclick = ev => {
+  ev.preventDefault();
+
+  if (inputElement.value) {
+
+    const textElement = document.createElement("span")
+    textElement.innerHTML = inputElement.value
+
+    const btnElement = document.createElement("button");
+    btnElement.innerHTML = "Remover";
+
+    const liElement = document.createElement("li");
+
+    liElement.appendChild(textElement);
+    liElement.appendChild(btnElement);
+    ulElement.appendChild(liElement);
+
+    btnElement.onclick = () => {
+      ulElement.removeChild(liElement);
+    }
+
+    inputElement.value = ""
+  }
+}
